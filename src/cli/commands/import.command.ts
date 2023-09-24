c3380e6a6f962441c2b8026a2f5a4740a618d69b
OksMor <sancho_2006_ru@mail.ru>
2023-09-24 16:00:56 +0300

    добавляет основу для импорта ImportCommand WIP

---
+++ src/cli/commands/import.command.ts
@@ -0,0 +1,11 @@
+import { Command } from './commands.interface.js';
+
+export class ImportCommand implements Command {
+  public getName(): string {
+    return '--import';
+  }
+
+  public execute(...parameters: string[]): void {
+    //чтение файла
+  }
+}
