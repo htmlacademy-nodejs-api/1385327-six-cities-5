ca8d0d750ffe7bfe10f04917e2f6f3e93e08c6d9
OksMor <Sancho_2006_ru@mail.ru>
2023-10-03 15:20:42 +0300

    добавляет потоки на запись

--- src/cli/commands/generate.command.ts
+++ src/cli/commands/generate.command.ts
@@ -1,8 +1,10 @@
 import got from 'got';
-import { appendFile } from 'node:fs/promises';
+// import { appendFile } from 'node:fs/promises';
 import { Command } from './commands.interface.js';
 import { MockServerData } from '../../shared/types/mock-server-data.type.js';
 import { TSVOfferGenerator } from '../../shared/libs/offer-generator/tsv-offer-generator.js';
+import { getErrorMessage } from '../../shared/helpers/index.js';
+import { TSVFileWriter } from '../../shared/libs/file-writer/index.js';
 
 export class GenerateCommand implements Command {
 
@@ -18,12 +20,16 @@ export class GenerateCommand implements Command {
 
   private async write(filepath: string, offerCount: number) {
     const tsvOfferGenerator = new TSVOfferGenerator(this.initialData);
+    const tsvFileWriter = new TSVFileWriter(filepath);
+
     for (let i = 0; i < offerCount; i++) {
-      await appendFile(
-        filepath,
-        `${tsvOfferGenerator.generate()}\n`,
-        { encoding: 'utf8' }
-      );
+    //   await appendFile(
+    //     filepath,
+    //     `${tsvOfferGenerator.generate()}\n`,
+    //     { encoding: 'utf8' }
+    //   );
+    // }
+      await tsvFileWriter.write(tsvOfferGenerator.generate());
     }
   }
 
@@ -42,9 +48,10 @@ export class GenerateCommand implements Command {
     } catch (error: unknown) {
       console.error('Can\'t generate data');
 
-      if (error instanceof Error) {
-        console.error(error.message);
-      }
+      // if (error instanceof Error) {
+      //   console.error(error.message);
+      // }
+      console.error(getErrorMessage(error));
     }
   }
 }
