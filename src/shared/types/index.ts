9c0c12cd9cce31e570deaf6c1e5ec096616f8d7a
OksMor <Sancho_2006_ru@mail.ru>
2023-10-10 11:27:12 +0300

    добавляет модель user способ для mongoose

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -4,3 +4,4 @@ export * from './housing.enum.js';
 export * from './feature.enum.js';
 export * from './mock-server-data.type.js';
 export * from './component.enum.js';
+export * from './user.type.js';
