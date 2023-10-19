33d41ffe31f1cbfe61f669180c989a79bcc530da
OksMor <Sancho_2006_ru@mail.ru>
2023-10-19 11:24:20 +0300

    добавляет express

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,21 +1,25 @@
 import { inject, injectable } from 'inversify';
+import express, { Express } from 'express';
 import { Logger } from '../shared/libs/logger/index.js';
 import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { Component } from '../shared/types/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
-import { OfferService } from '../shared/modules/offer/offer-service.interface.js';
+// import { OfferService } from '../shared/modules/offer/offer-service.interface.js';
 // import { UserModel } from '../shared/modules/user/index.js';
 
 @injectable()
 export class RestApplication {
+  private server: Express;
 
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DatabaseClient) private readonly databaseClient: DatabaseClient,
-    @inject(Component.OfferService) private readonly offerService: OfferService,
-  ) {}
+    // @inject(Component.OfferService) private readonly offerService: OfferService,
+  ) {
+    this.server = express();
+  }
 
   private async _initDb() {
     const mongoUri = getMongoURI(
@@ -29,27 +33,28 @@ export class RestApplication {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     // console.log(process.env);
     this.logger.info('Application initialization');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);// для демонстрации
+    // this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
 
     this.logger.info('Init database...');
     await this._initDb();
 
-    // const user = await UserModel.create({
-    //   email: 'test@email.local',
-    //   avatar: 'keks.jpg',
-    //   name: 'Keks',
-    //   type: 'Unknown'
-    //  });
-    //  console.log(user);
-
     this.logger.info('Init database completed');
 
+    this.logger.info('Try to init server...');
+    await this._initServer();
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
+
     // const result = await this.offerService.findById('652fa3d32b729346360f5304');
-    const result = await this.offerService.findPremiumByCityName('Cologne');
-    console.log(result);
+    // const result = await this.offerService.findPremiumByCityName('Cologne');
+    // console.log(result);
 
   }
 }
